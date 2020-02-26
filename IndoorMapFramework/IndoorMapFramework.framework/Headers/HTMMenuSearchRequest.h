4209eb49411107acab25e6ce931a74ccb8ecb1c5
Dast1 <myl90@126.com>
2020-02-26 10:20:17 +0800

    版本更新说明v0.70.1:适配菜单接口新规则

--- IndoorMapFramework/IndoorMapFramework.framework/Headers/HTMMenuSearchRequest.h
+++ IndoorMapFramework/IndoorMapFramework.framework/Headers/HTMMenuSearchRequest.h
@@ -11,11 +11,14 @@
 NS_ASSUME_NONNULL_BEGIN
 
 
-/// 根据当前位置获取POI菜单
+/// 根据当前位置获取对应app版本的城市菜单，支持返回多个页面的菜单
 @interface HTMMenuSearchRequest : NSObject
 
 /// 经纬度
 @property(nonatomic, assign) CLLocationCoordinate2D coor2D;
+
+//app版本,如"wheelchair"
+@property(nonatomic, copy) NSString *version;
 @end
 
 NS_ASSUME_NONNULL_END
